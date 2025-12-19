881bbad80f4e988c9493df8d0e5f19e00800ae56
biocodeit <glencoelho135@gmail.com>
2025-12-19 10:35:41 +0530

    parallel execution is not desired

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,7 @@ import { getenvVar } from './api-testing/helpers/env-setup';
 export default defineConfig({
   testDir: './api-testing',
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
